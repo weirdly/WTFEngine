df9996522ac9df4f7615509a90bfede38c363cca
Malcolm Fell <emarref@gmail.com>
2016-04-04 14:00:07 +1200

    Use Weirdly spreadsheet

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,6 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+    WTF.init( '15bN-Wx8qt9Hs8gwQR04-s87ZtP8ZrUQ5KEtyd_JTuvg' );
 
 });
