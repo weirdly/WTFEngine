34c755d6b3dfc03ab84f0370cf52b3bd0dfef589
Malcolm Fell <emarref@gmail.com>
2016-04-06 09:23:40 +1200

    Add button functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,26 @@
 import {default as $} from 'npm-zepto';
 import WTF from './wtf';
 
+class WG {
+    constructor() {
+        WTF.init('15bN-Wx8qt9Hs8gwQR04-s87ZtP8ZrUQ5KEtyd_JTuvg');
+        this.initEvents();
+    }
+
+    initEvents() {
+        $(document).on('click', 'a#start',         (e) => {e.preventDefault(), this.showPage('.page-generator');});
+        $(document).on('click', 'a#new-challenge', (e) => {e.preventDefault(), WTF.generate();});
+        $(document).on('click', 'a#about',         (e) => {e.preventDefault(), this.showPage('.page-about');});
+    }
+
+    showPage(page) {
+        $('.page').addClass('hidden');
+        $(page).removeClass('hidden');
+    }
+};
+
+var wg;
+
 $(function() {
-    WTF.init( '15bN-Wx8qt9Hs8gwQR04-s87ZtP8ZrUQ5KEtyd_JTuvg' );
-});
-
-$(document).on('click', 'a#start', function () {
-    $('.page').addClass('hidden');
-    $('.page-generator').removeClass('hidden');
-});
-
-$(document).on('click', 'a#new-challenge', function (e) {
-    e.preventDefault();
-    WTF.generate();
+    wg = new WG();
 });
