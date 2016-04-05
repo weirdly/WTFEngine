18adc8d0663aea74c919091fd0a90d7979fb8cc9
Malcolm Fell <emarref@gmail.com>
2016-04-05 13:56:40 +1200

    Update 2016-04-05T01:56:25.927Z

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-
-$(function() {
-    // Populate using a Google spreadsheet ID (you must publish it first!)
-    // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '15bN-Wx8qt9Hs8gwQR04-s87ZtP8ZrUQ5KEtyd_JTuvg' );
-});
